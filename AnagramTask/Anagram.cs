595c3f03275823b9929ba1be08c4646a598d6744
Yury <mr.deusvult@gmail.com>
2021-04-07 21:42:13 +0300

    up

--- AnagramTask/Anagram.cs
+++ AnagramTask/Anagram.cs
@@ -1,9 +1,13 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AnagramTask
 {
     public class Anagram
     {
+        public string SourceWord { get; set; }
+        public List<string> list = new List<string>();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Anagram"/> class.
         /// </summary>
@@ -12,7 +16,17 @@ namespace AnagramTask
         /// <exception cref="ArgumentException">Thrown when  source word is empty.</exception>
         public Anagram(string sourceWord)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (sourceWord is null)
+            {
+                throw new ArgumentNullException("word is null");
+            }
+
+            if (sourceWord.Length == 0)
+            {
+                throw new ArgumentException("word is empty");
+            }
+
+            SourceWord = sourceWord;
         }
 
         /// <summary>
@@ -23,7 +37,49 @@ namespace AnagramTask
         /// <exception cref="ArgumentNullException">Thrown when candidates list is null.</exception>
         public string[] FindAnagrams(string[] candidates)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (candidates is null)
+            {
+                throw new ArgumentNullException("array is null");
+            }
+
+            foreach (string candidate in candidates)
+            {
+                if (IsAnagram(SourceWord.ToLower(), candidate.ToLower()))
+                {
+                    list.Add(candidate);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        private bool IsAnagram(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            if (s1 == s2)
+            {
+                return false;
+            }
+
+            foreach (char c in s2)
+            {
+                int x = s1.IndexOf(c);
+
+                if (x >= 0)
+                {
+                    s1 = s1.Remove(x, 1);
+                }
+                else
+                {
+                    return false;
+                }          
+            }
+
+            return string.IsNullOrEmpty(s1);
         }
     }
 }
